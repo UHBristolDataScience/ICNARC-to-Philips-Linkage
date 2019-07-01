79b289d5803da96d9172139315ac22a43e8b838d
Rusty <cm1788@bristol.ac.uk>
2019-07-01 12:31:10 +0200

    Added code to produce clean figures for publication. TODO: remove anomalous and non-physical variable values.

--- clean_encounterids.py
+++ clean_encounterids.py
@@ -198,7 +198,98 @@ def combine_non_unique_encounters(merged_data, combine='concat', verbose=True):
 
 	return groups
 
-############# We now want to link to ICNARC XML and clean up (document in standard python format) and save results.....
+convert_minutes_to_days = lambda x: x/float(24*60)
+
+def print_philips_summary(df):
+	'''Summary of the variables that were extracted from Philips ICCA (rather than ICNARC).'''
+
+	median_age = np.median(df['age'].values)
+	age_q25, age_q75 = np.percentile(df['age'].values, [25,75])
+	print "Age, median years (IQR): %.1f (%.1f, %.1f)" %(median_age,age_q25,age_q75) 
+
+	median_los = convert_minutes_to_days(np.median(df['lengthOfStay (mins)'].values))
+	los_q25, los_q75 = map(convert_minutes_to_days, np.percentile(df['lengthOfStay (mins)'].values, [25,75]))
+	print "LOS, median days (IQR): %.1f (%.1f, %.1f)" %(median_los,los_q25,los_q75)
+	print ""
+
+	n_male = sum(df['gender']=='Male')
+	n_female = sum(df['gender']=='Female')
+	no_gender = sum(df['gender'].isna())    
+	print "Gender, %% female: %.1f" %(100 * n_female / float(n_male + n_female))
+	print "(%.1f %% of patients have no gender recorded in Philips.)" %(100*no_gender/float(len(df)))
+	print " "
+
+def print_icnarc_summary(df):
+	'''Summary of the equivalent variables from ICNARC.
+	NOte: these values are trusted over the Philips ones because they have been validated by humans.
+	'''
+
+	median_age = np.median(df['icnarc_age'].values)
+	age_q25, age_q75 = np.percentile(df['icnarc_age'].values, [25,75])
+	print "Age, median years (IQR): %.1f (%.1f, %.1f)" %(median_age,age_q25,age_q75) 
+
+	median_los = convert_minutes_to_days(np.median(df['icnarc_los'].values))
+	los_q25, los_q75 = map(convert_minutes_to_days, np.percentile(df['icnarc_los'].values, [25,75]))
+	print "LOS, median days (IQR): %.1f (%.1f, %.1f)" %(median_los,los_q25,los_q75)
+	print ""
+
+	n_male = sum(df['icnarc_gender']=='Male')
+	n_female = sum(df['icnarc_gender']=='Female')
+	no_gender = sum(df['icnarc_gender'].isna())    
+	print "Gender, %% female: %.1f" %(100 * n_female / float(n_male + n_female))
+	print "(%.1f %% of patients have no gender recorded in Philips.)" %(100*no_gender/float(len(df)))
+	print " "
+
+	readmit = sum(df['Readmission during this hospital stay']=='Yes')
+	no_readmit = sum(df['Readmission during this hospital stay'].isna())
+	print "Readmission to ICU, #(%%) : %d (%.1f)" %(readmit, 100*readmit/float(len(df)))
+	print "(%.1f %% of patients have no recording for this variable in WW.)" %(100*no_readmit/float(len(df)))
+
+
+def combine_date_time_columns(df, columns, drop_originals=False):
+	'''ICNARC CMP stores separate date and time variables. We
+	This method combines them and converts the string into a python datteime.
+	It also optionally drops the original colums.'''
+
+	for col in columns:	
+		df[col[0].replace('Date', 'Datetime')] = pd.to_datetime(df[col[0]].map(str ) + ' ' + df[col[1]], infer_datetime_format=True)
+		if drop_originals:		
+			df = df.drop(labels=list(col), axis=1)
+		
+	return df
+
+def calculate_icnarc_outtime(df):
+	'''There are a two ways the patient can leave the ICU, which recorded according to ICNARC's non-duplication data model:
+		- discharge
+		- body removed (following death or declaration of brainstem death)
+
+	This method combines these different datetime columns into a single outTime_icnarc column.
+	Datetime columns must first be combined wiht method combine_date_time_columns prior to calling.
+	'''
+	df['icnarc_outTime'] = [row['Datetime body removed from your unit'] if pd.isnull(row['Datetime of discharge from your unit']) 
+				else row['Datetime of discharge from your unit'] for i,row in df.iterrows()]
+	if sum(df['icnarc_outTime'].apply(pd.isnull))>0:
+		print "Warning: could not calculate outTime_icnarc for all patients."
+	return df
+
+def calculate_icnarc_in_hospital_mortality(df):
+	'''This maps the ICNARC variables DIS, HDIS, UHDIS, UDIS to a single in-hopistal mortality measure.'''
+	new_colum = []
+	for i,row in df.iterrows():
+
+		ultimate = row['Status at ultimate discharge from hospital']
+		this_hospital = row['Status at discharge from your hospital']
+		this_unit = row['Status at discharge from your unit']
+
+		if not pd.isnull(ultimate):
+			new_colum.append(ultimate)
+		elif not pd.isnull(this_hospital):
+			new_colum.append(this_hospital)
+		elif not pd.isnull(this_unit):
+			new_colum.append(this_unit)
+
+	df['icnarc_in_hospital_mortality'] = new_colum
+	return df
 
 if __name__=="__main__":
 
